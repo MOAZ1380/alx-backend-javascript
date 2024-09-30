1d11642fc4995049662035b8c536f85390fb5864
Moaz Sayed <moaz469654@gmail.com>
2024-10-01 00:53:58 +0200

    f

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -4,7 +4,7 @@ export default function createReportObject(employeesList) {
       ...employeesList,
     },
   getNumberOfDepartments(employeesList) {
-    return Object.keys(this.employeesList).length;
+    return Object.keys(employeesList).length;
   },
   };
 }
