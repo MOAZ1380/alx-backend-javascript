809efdb106e8682c1c121a37814b235bb78cde5f
moaz <moaz469654@gmail.com>
2024-10-09 23:50:02 +0000

    moaz

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,54 +1,62 @@
 interface DirectorInterface {
-    workFromHome(): string
-
-    getCoffeeBreak(): string
-
-    workDirectorTasks(): string
+  workFromHome():string;
+  getCoffeeBreak():string;
+  workDirectorTasks():string;
 }
+
 
 interface TeacherInterface {
-    workFromHome(): string
-
-    getCoffeeBreak(): string
-
-    workTeacherTasks(): string
+  workFromHome():string;
+  getCoffeeBreak():string;
+  workTeacherTasks():string;
 }
 
-export class Director implements DirectorInterface {
 
-    getCoffeeBreak = (): string => "Getting a coffee break";
-
-    workDirectorTasks = (): string => "Getting to director tasks";
-
-    workFromHome = (): string => "Working from home";
-
+class Director implements DirectorInterface{
+  workFromHome = ():string => "Working from home";
+  getCoffeeBreak = ():string => "Getting a coffee break";
+  workDirectorTasks = ():string => "Getting to director tasks";
 }
 
-export class Teacher implements TeacherInterface {
+class Teacher implements TeacherInterface {
     getCoffeeBreak = (): string => "Cannot have a break";
-
     workFromHome = (): string => "Cannot work from home";
-
     workTeacherTasks = (): string => "Getting to work";
-
 }
 
-export const createEmployee = (salary: number | string): Teacher | Director => Number(salary) < 500 ? new Teacher() : new Director();
-
-export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
-    return (employee as Director).workDirectorTasks() !== undefined;
+function createEmployee(salary: number | string): Teacher | Director {
+  return Number(salary) < 500 ? new Teacher() : new Director();
 }
 
-export function executeWork(employee: DirectorInterface | TeacherInterface): string {
-    let res = undefined;
-    (isDirector(employee)) ? res = employee.workDirectorTasks() : res = employee.workTeacherTasks();
-    return res;
+
+function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: DirectorInterface | TeacherInterface) {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
 }
 
 type Subjects = "Math" | "History";
 
-// @ts-ignore
-// @ts-ignore
-export function teachClass(todayClass: Subjects): string | boolean {
-    return (todayClass === "Math") ? "Teaching Math" : (todayClass === "History") ? "Teaching History" : `${undefined}`
+
+function teachClass(todayClass: Subjects): string{
+  if(todayClass === "Math"){
+    return "Teaching Math";
+  }
+  else{
+    return "Teaching History";
+  }
 }
+
+console.log(executeWork(createEmployee(200)));   // Output: "Getting to work"
+console.log(executeWork(createEmployee(1000)));  // Output: "Getting to director tasks"
+
+console.log(teachClass('Math'));    // Output: "Teaching Math"
+console.log(teachClass('History')); // Output: "Teaching History"
+
+
